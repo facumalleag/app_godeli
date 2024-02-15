72d28595e28de8adcf34f229628796bc56caae93
facumalleag <39714914+facumalleag@users.noreply.github.com>
2024-02-15 19:09:57 -0300

    Delete godeli/godeli directory

--- godeli/godeli/redux/Store.tsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterSlice from './slices/CounterSlices';
-const store = configureStore({
-    reducer: {
-        counter: counterSlice.reducer,
-        // otros slices
-    }
-});
-export default store;
