a3ababc905cc68631df3ca6b0bc2e10df10bdcc9
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2024-02-16 18:55:28 -0300

    Se agrega el boton de Google al login, se modifica aspectos visuales

--- godeli/src/Login.tsx
+++ godeli/src/Login.tsx
@@ -1,16 +1,57 @@
-import React, { useEffect } from 'react';
-import { View, ImageBackground, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ImageBackground, Image, Text, TouchableOpacity, Button, StyleSheet } from 'react-native';
 import NetworkController from '../controller/NetworkController';
 import { styles } from '../theme/LandingStyle';
 import InternetAlert from '../components/InternetAlert';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 
+const configureGoogleSignIn = () => {
+  GoogleSignin.configure({
+    webClientId:
+      "441344649242-h0sfbuk4m7al7lr5o0i69s3rvnt9j74m.apps.googleusercontent.com",
+    androidClientId:
+      "441344649242-vbs53htmmmpd9sqs46nmtrt1qujfp0nq.apps.googleusercontent.com",
+    iosClientId:
+      "441344649242-ie1h43u7hl6b9676vhetiom8qninilsr.apps.googleusercontent.com",
+  });
+};
 
 interface Props extends NativeStackScreenProps<any,any>{
   
 }
 
 export default function Login({navigation}:Props) {
+  const [error, setError] = useState();
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    configureGoogleSignIn();
+  });
+
+  const signIn = async () => {
+    console.log("Pressed sign in");
+  
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      setUserInfo(userInfo);
+      setError();
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  const logout = () => {
+    setUserInfo(undefined);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+  };
+
    useEffect(() => {
     const checkConnection =  async () => {
       const isConnected = await NetworkController.checkInternetConnection();
@@ -33,24 +74,51 @@ export default function Login({navigation}:Props) {
     checkConnection();
   },[]);
 
-
-
-  return (
-   <View style={styles.container}>
-      <ImageBackground source={require('../assets/background.png')} style={styles.backgroundImage} >
-        <View style={styles.logoContainer}>
-          <Image source={require('../assets/logo.png')} style={styles.logo} />
+return (
+    <View style={stylesLogin.container}>
+      <ImageBackground source={require('../assets/background.png')} resizeMode="cover" style={stylesLogin.image} >
+        <View style={stylesLogin.containerBody}>
+          <View style={styles.logoContainer}>
+            <Image source={require('../assets/logo.png')} style={styles.logo} />
+          </View>
+          <View style={{ alignSelf: 'center', marginRight: 40 }}>
+            <Text style={styles.greetingText}>Hola,</Text>
+            <Text style={styles.welcomeText}>Genial verte de nuevo!</Text>
+          </View>
+          <View style={stylesLogin.buttonGoogle}>
+            { userInfo ? (
+              <Button title="Logout" onPress={logout} />
+            ) : (
+              <GoogleSigninButton
+              size={GoogleSigninButton.Size.Standard}
+              color={GoogleSigninButton.Color.Dark}
+              onPress={signIn}
+              />
+            )}
+            
+          </View>
         </View>
-        <View style={{ alignSelf: 'center', marginRight: 40 }}>
-          <Text style={styles.greetingText}>Hola,</Text>
-          <Text style={styles.welcomeText}>Genial verte de nuevo!</Text>
-        </View>
-        <TouchableOpacity style={styles.googleButton}
-         onPress={() => navigation.navigate('TabNavigator')}
-        >
-          <Text style={styles.buttonText}>Iniciar sesión con Google</Text>
-        </TouchableOpacity>
       </ImageBackground>
-    </View> 
+    </View>
   );
 }
+
+const stylesLogin = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerBody: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    paddingBottom: 100
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonGoogle: {
+    alignSelf: 'center',
+    marginTop: 100,
+  },
+});
